4ae299f544f9f40311f038ce319c89673d500386
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-03 11:21:43 +0000

    Code refactoring

--- frontend/src/components/ClassifierResult.js
+++ frontend/src/components/ClassifierResult.js
@@ -7,83 +7,83 @@ import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
 
 const ClassifierResult = ({ selectedImage, classificationResult }) => {
-    const theme = useTheme();
-
-    return (
-        <React.Fragment>
-            <Grid item xs={12}>
-                <Card>
-                    <CardContent>
-                        <Box>
-                            <Box
-                                display='flex'
-                                justifyContent='center'
-                                marginBottom={2} 
-                                marginTop={1}
-                            >
-                                <Typography 
-                                    variant='h2' 
-                                    align='center' 
-                                    gutterBottom
-                                >
-                                    Result
-                                </Typography>
-                            </Box>
-                            <Box 
-                                flexDirection='flex'
-                                justifyContent='center'
-                            >
-                                <Typography 
-                                    variant='subtitle1' 
-                                    align='center' 
-                                    gutterBottom
-                                >
-                                    The image you have selected:
-                                    <br /><br />
-                                </Typography>
-                                <Box
-                                    sx={{
-                                        transition: 'all .2s ease-in-out',
-                                        '&:hover': {
-                                            transform: `translateY(-${theme.spacing(1 / 2)})`
-                                        }
-                                    }}
-                                >
-                                    <img 
-                                        src={selectedImage} 
-                                        height='250'
-                                        style={{
-                                            display: 'block',
-                                            margin: '0 auto',
-                                            boxShadow: '6px 6px 3px #c5c5c5',
-                                            borderRadius: '25px'
-                                        }} 
-                                    />
-                                </Box>
-                                <br /><br />
-                                <Typography 
-                                    variant='subtitle1' 
-                                    align='center' 
-                                    gutterBottom
-                                >
-                                    The machine learning model has classified this image as:
-                                    <br /><br />
-                                </Typography>
-                                <Typography 
-                                    variant='h3' 
-                                    color='primary'
-                                    align='center' 
-                                    gutterBottom
-                                >
-                                    {classificationResult}
-                                </Typography>
-                            </Box>
-                        </Box>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </React.Fragment>
-    );
+  const theme = useTheme();
+  
+  return (
+    <>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent>
+            <Box>
+              <Box
+                display='flex'
+                justifyContent='center'
+                marginBottom={2} 
+                marginTop={1}
+              >
+                <Typography 
+                  variant='h2' 
+                  align='center' 
+                  gutterBottom
+                >
+                  Result
+                </Typography>
+              </Box>
+              <Box 
+                flexDirection='flex'
+                justifyContent='center'
+              >
+                <Typography 
+                  variant='subtitle1' 
+                  align='center' 
+                  gutterBottom
+                >
+                  The image you have selected:
+                  <br /><br />
+                </Typography>
+                <Box
+                  sx={{
+                    transition: 'all .2s ease-in-out',
+                    '&:hover': {
+                      transform: `translateY(-${theme.spacing(1 / 2)})`
+                    }
+                  }}
+                >
+                  <img 
+                    src={selectedImage} 
+                    height='250'
+                    style={{
+                      display: 'block',
+                      margin: '0 auto',
+                      boxShadow: '6px 6px 3px #c5c5c5',
+                      borderRadius: '25px'
+                    }} 
+                  />
+                </Box>
+                <br /><br />
+                <Typography 
+                  variant='subtitle1' 
+                  align='center' 
+                  gutterBottom
+                >
+                  The machine learning model has classified this image as:
+                  <br /><br />
+                </Typography>
+                <Typography 
+                  variant='h3' 
+                  color='primary'
+                  align='center' 
+                  gutterBottom
+                >
+                  {classificationResult}
+                </Typography>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
 };
 
 export default ClassifierResult;
--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -11,73 +11,73 @@ import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
 import DescriptionItem from './DescriptionItem';
 
 const Description = () => {
-    const theme = useTheme();
-
-    return (
-        <Box
-            maxWidth={{ sm: 720, md: 1236 }}
-            width={1}
-            margin='0 auto'
-            paddingTop={2}
-            paddingBottom={2}
+  const theme = useTheme();
+  
+  return (
+    <Box
+      maxWidth={{ sm: 720, md: 1236 }}
+      width={1}
+      margin='0 auto'
+      paddingTop={2}
+      paddingBottom={2}
+    >
+      <Box
+        data-aos='fade-up'
+        backgroundColor={theme.palette.background.default}
+        paddingTop={4}
+      >
+        <Container
+          maxWidth='lg'
+          display='flex'
+          sx={{
+            alignItems: 'center',
+            flexDirection: 'column',
+            px: {
+              md: '15px !important'
+            }
+          }}
         >
-            <Box
-                data-aos='fade-up'
-                backgroundColor={theme.palette.background.default}
-                paddingTop={4}
-            >
-                <Container
-                    maxWidth='lg'
-                    display='flex'
-                    sx={{
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                        px: {
-                            md: '15px !important'
-                        }
-                    }}
-                >
-                    <Typography
-                        data-aos='fade-up'
-                        align='center'
-                        color={theme.palette.text.primary}
-                        variant='h1'
-                        marginTop='30px'
-                    >
-                        How Does It Work?
-                    </Typography>
-                    <Typography
-                        data-aos='fade-up'
-                        align='center'
-                        color={theme.palette.text.secondary}
-                        variant='h4'
-                        paddingTop={3}
-                        paddingBottom={3}
-                        marginBottom='15px'
-                    >
-                        A step-by-step guide on how to use the app
-                    </Typography>
-                    <Grid container spacing={4} data-aos='fade-up'>
-                        <DescriptionItem 
-                            icon={<AddAPhotoOutlinedIcon style={{ height: 25, width: 25 }} />}
-                            title='Select an Image'
-                            subtitle='Select an image for classification and drag-and-drop it into the provided dropzone in the browser.'
-                        />
-                        <DescriptionItem 
-                            icon={<SendToMobileOutlinedIcon style={{ height: 25, width: 25 }} />}
-                            title='Send the Image for Classification'
-                            subtitle='Press the Send Image button to send the image to the machine learning model for classification.'
-                        />
-                        <DescriptionItem 
-                            icon={<GetAppOutlinedIcon style={{ height: 25, width: 25 }} />}
-                            title='Get the Classification Result'
-                            subtitle='Next, the machine learning model classifies the image, and the result of the classification is then printed on the screen.'
-                        />
-                    </Grid>
-                </Container>
-            </Box>
-        </Box>
-    );
+          <Typography
+            data-aos='fade-up'
+            align='center'
+            color={theme.palette.text.primary}
+            variant='h1'
+            marginTop='30px'
+          >
+            How Does It Work?
+          </Typography>
+          <Typography
+            data-aos='fade-up'
+            align='center'
+            color={theme.palette.text.secondary}
+            variant='h4'
+            paddingTop={3}
+            paddingBottom={3}
+            marginBottom='15px'
+          >
+            A step-by-step guide on how to use the app
+          </Typography>
+          <Grid container spacing={4} data-aos='fade-up'>
+            <DescriptionItem 
+              icon={<AddAPhotoOutlinedIcon style={{ height: 25, width: 25 }} />}
+              title='Select an Image'
+              subtitle='Select an image for classification and drag-and-drop it into the provided dropzone in the browser.'
+            />
+            <DescriptionItem 
+              icon={<SendToMobileOutlinedIcon style={{ height: 25, width: 25 }} />}
+              title='Send the Image for Classification'
+              subtitle='Press the Send Image button to send the image to the machine learning model for classification.'
+            />
+            <DescriptionItem 
+              icon={<GetAppOutlinedIcon style={{ height: 25, width: 25 }} />}
+              title='Get the Classification Result'
+              subtitle='Next, the machine learning model classifies the image, and the result of the classification is then printed on the screen.'
+            />
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 export default Description;
