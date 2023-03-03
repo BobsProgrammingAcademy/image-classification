ab76d1c35fb967e5fd98c1d6a3c9d1c1a1f44e58
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-03 23:55:51 +0000

    Code refactoring

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -11,123 +11,122 @@ import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
 
 const Footer = () => {
-    const theme = useTheme();
-
-    return (
-        <React.Fragment>
-            <Box
-                backgroundColor={theme.palette.background.default}
-                paddingTop='1px'
-                paddingBottom='1px'
-                // sticky footer - see four values below
-                position='fixed'
-                bottom='0'
-                left='0'
-                width='100%'
-            >
-                <Divider />
-                <Box
-                    backgroundColor={theme.palette.background.default}
-                    position='relative'
-                    padding={theme.spacing(0.25)}
+  const theme = useTheme();
+  
+  return (
+    <>
+      <Box
+        backgroundColor={theme.palette.background.default}
+        paddingTop='1px'
+        paddingBottom='1px'
+        // sticky footer - see four values below
+        position='fixed'
+        bottom='0'
+        left='0'
+        width='100%'
+      >
+        <Divider />
+        <Box
+          backgroundColor={theme.palette.background.default}
+          position='relative'
+          padding={theme.spacing(0.25)}
+        >
+          <Grid container spacing={0}>
+            <Hidden mdDown>
+              <Grid container item xs={12} md={4}>
+                <List
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    padding: 0
+                  }}
                 >
-                    <Grid container spacing={0}>
-                        <Hidden mdDown>
-                            <Grid container item xs={12} md={4}>
-                                <List
-                                    sx={{
-                                        display: 'flex',
-                                        flexDirection: 'row',
-                                        padding: 0
-                                    }}
-                                >
-                                    <ListItemButton component='a' href='#'>
-                                        <ListItemText 
-                                            primary={
-                                                <Typography 
-                                                    variant='body2'
-                                                    color={theme.palette.text.secondary}
-                                                >
-                                                    Privacy Policy
-                                                </Typography>
-                                            }
-                                        />
-                                    </ListItemButton>
-                                    <ListItemButton component='a' href='#'>
-                                        <ListItemText 
-                                            primary={
-                                                <Typography 
-                                                    variant='body2'
-                                                    color={theme.palette.text.secondary}
-                                                >
-                                                    Terms of Use
-                                                </Typography>
-                                            }
-                                        />
-                                    </ListItemButton>
-                                </List>
-                            </Grid>
-                        </Hidden>
-                        <Grid container item xs={12} md={4} justifyContent='center'>
-                            <List>
-                                <ListItemButton>
-                                    <ListItemText 
-                                        primary={
-                                            <Typography 
-                                                variant='body2'
-                                                color={theme.palette.text.secondary}
-                                            >
-                                                Copyright &copy; {new Date().getFullYear()} Bob's Programming Academy.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </List>
-                        </Grid>
-
-                        <Grid container item xs={12} md={4} justifyContent='center'>
-                            <List>
-                                <ListItemButton>
-                                    <ListItemText 
-                                        primary={
-                                            <Typography 
-                                                variant='body2'
-                                                color={theme.palette.text.secondary}
-                                            >
-                                                Photo by
-                                                {' '}
-                                                <Link 
-                                                    href='https://unsplash.com/@joshwp'
-                                                    alt='Josh Power'
-                                                    target='_blank'
-                                                    rel='noreferrer'
-                                                    color={theme.palette.text.secondary}
-                                                >
-                                                    Josh Power
-                                                </Link>
-                                                {' '}
-                                                on
-                                                {' '} 
-                                                <Link 
-                                                    href='https://unsplash.com/photos/FWoH2qhLb5I'
-                                                    alt='Unsplash'
-                                                    target='_blank'
-                                                    rel='noreferrer'
-                                                    color={theme.palette.text.secondary}
-                                                >
-                                                    Unsplash
-                                                </Link>.
-                                            </Typography>
-                                        }
-                                    />
-                                </ListItemButton>
-                            </List>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
-        </React.Fragment>
-    );
+                  <ListItemButton component='a' href='#'>
+                    <ListItemText 
+                      primary={
+                        <Typography 
+                          variant='body2'
+                          color={theme.palette.text.secondary}
+                        >
+                          Privacy Policy
+                        </Typography>
+                      }
+                    />
+                  </ListItemButton>
+                  <ListItemButton component='a' href='#'>
+                    <ListItemText 
+                      primary={
+                        <Typography 
+                          variant='body2'
+                          color={theme.palette.text.secondary}
+                        >
+                          Terms of Use
+                        </Typography>
+                      }
+                    />
+                  </ListItemButton>
+                </List>
+              </Grid>
+            </Hidden>
+            <Grid container item xs={12} md={4} justifyContent='center'>
+              <List>
+                <ListItemButton>
+                  <ListItemText 
+                    primary={
+                      <Typography 
+                        variant='body2'
+                        color={theme.palette.text.secondary}
+                      >
+                        Copyright &copy; {new Date().getFullYear()} Bob's Programming Academy.
+                      </Typography>
+                    }
+                  />
+                </ListItemButton>
+              </List>
+            </Grid>
+            <Grid container item xs={12} md={4} justifyContent='center'>
+              <List>
+                <ListItemButton>
+                  <ListItemText 
+                    primary={
+                      <Typography 
+                        variant='body2'
+                        color={theme.palette.text.secondary}
+                      >
+                        Photo by
+                        {' '}
+                        <Link 
+                          href='https://unsplash.com/@joshwp'
+                          alt='Josh Power'
+                          target='_blank'
+                          rel='noreferrer'
+                          color={theme.palette.text.secondary}
+                        >
+                          Josh Power
+                        </Link>
+                        {' '}
+                        on
+                        {' '} 
+                        <Link 
+                          href='https://unsplash.com/photos/FWoH2qhLb5I'
+                          alt='Unsplash'
+                          target='_blank'
+                          rel='noreferrer'
+                          color={theme.palette.text.secondary}
+                        >
+                          Unsplash
+                        </Link>.
+                      </Typography>
+                    }
+                  />
+                </ListItemButton>
+              </List>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Footer;
